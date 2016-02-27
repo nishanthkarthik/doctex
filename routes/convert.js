6c000ceb47bfab06a24f43e1e8ab0b3519e20152
nishanthkarthik <nishanthkarthik@live.com>
2016-02-27 23:02:23 +0530

    Added results

--- routes/convert.js
+++ routes/convert.js
@@ -1,19 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer')
-
-var storage = multer.diskStorage({
-	destination: function(req, file, callback) {
-		callback(null, './uploads');
-	},
-	filename: function(req, file, callback) {
-		callback(null, file.fieldname + '-' + Date.now());
-	}
-});
-
-var upload = multer({
-	storage: storage
-}).single('docfile');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,16 +10,36 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+/*POST conversion api*/
+var storage = multer.diskStorage({
+	destination: function(req, file, callback) {
+		callback(null, './uploads');
+	},
+	filename: function(req, file, callback) {
+		callback(null, file.originalname);
+	}
+});
+
+var upload = multer({
+	storage: storage
+}).single('userDoc');
+
 router.post('/', function(req, res) {
 	upload(req, res, function(err) {
 		if (err)
 			return res.json({
-				error: 'error uploading file'
+				error: 'error uploading file',
+				detail: err
 			});
-		res.json({
-			message: 'uploaded',
-			result_url: 'to be implemented'
+
+		//process files here
+		fs.rename(req.file.originalname, req.file.originalname + '.tex', function(err) {
+			res.json({
+				message: 'uploaded',
+				result_url: '/result?id=' + req.file.originalname + '.tex'
+			});
 		});
+
 	});
 });
 
--- routes/result.js
+++ routes/result.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.end('Hello world');
+	console.log(JSON.stringify(req.query));
+	if (_.has(req.query, 'id') && req.query.id) {
+		var filename = __dirname + '/uploads/' + req.body.id;
+		fs.stat(filename, function(err, stats) {
+			if (err)
+				console.log(err);
+			if (stats.isFile())
+				res.download(filename);
+			else
+				res.json({error: 'file not available'});
+		});
+	}
+	res.json({
+		error: 'no id parameter specified'
+	});
 });
 
 module.exports = router;

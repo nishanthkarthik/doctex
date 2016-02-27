f84d3da3983040f8ae07cab4f0820e7111990c48
nishanthkarthik <nishanthkarthik@live.com>
2016-02-28 00:52:29 +0530

    mod sh

--- doctex.sh
+++ doctex.sh
@@ -4,5 +4,9 @@
 # [arg 2] is output dir, complete path
 # [arg 3] is output file, complete path
 
+echo 'input: ' $1
+echo 'out dir: ' $2
+echo 'out file: ' $3
+
 loffice --headless --convert-to odt --outdir $2 $1
 w2l -ultraclean ${$1/'doc'/'odt'} ${$1/'doc'/'docx.tex'}

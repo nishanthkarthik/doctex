2d1ea70d251b090593aaff030958257321a13428
nishanthkarthik <nishanthkarthik@live.com>
2016-02-27 23:47:24 +0530

    fixed results

--- routes/result.js
+++ routes/result.js
@@ -2,24 +2,33 @@ var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
 var fs = require('fs');
+var path = require('path');
+var appDir = path.dirname(require.main.filename).replace('bin','uploads/');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log(JSON.stringify(req.query));
+	console.log(appDir);
 	if (_.has(req.query, 'id') && req.query.id) {
-		var filename = __dirname + '/uploads/' + req.body.id;
+		var filename = appDir + req.query.id;
 		fs.stat(filename, function(err, stats) {
-			if (err)
+			if (err) {
 				console.log(err);
-			if (stats.isFile())
+				res.json({
+					error: err
+				});
+			} else if (err == null)
 				res.download(filename);
 			else
-				res.json({error: 'file not available'});
+				res.json({
+					error: 'file not available'
+				});
+		});
+	} else {
+		res.json({
+			error: 'no id parameter specified'
 		});
 	}
-	res.json({
-		error: 'no id parameter specified'
-	});
 });
 
 module.exports = router;

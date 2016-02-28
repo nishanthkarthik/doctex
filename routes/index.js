c2daf3fd09afb510b6699bc7f339edae8ace6e2f
nishanthkarthik <nishanthkarthik@live.com>
2016-02-28 10:40:33 +0530

    added back to home button

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'doctex' });
+  res.render('index', { title: 'docteX' });
 });
 
 module.exports = router;
